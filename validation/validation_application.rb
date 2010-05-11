3e01cb47054d16a3fe3c10a0ef5a92d886f2566f
mguetlein <martin.guetlein@gmail.com>
2010-05-11 18:51:04 +0200

    add search functionality plus example

--- validation/validation_application.rb
+++ validation/validation_application.rb
@@ -10,7 +10,7 @@ get '/crossvalidation/?' do
   LOGGER.info "list all crossvalidations"
   
   content_type "text/uri-list"
-  Validation::Crossvalidation.all.collect{ |d| url_for("/crossvalidation/", :full) + d.id.to_s }.join("\n")
+  Validation::Crossvalidation.all(params).collect{ |d| url_for("/crossvalidation/", :full) + d.id.to_s }.join("\n")
 end
 
 post '/crossvalidation/loo/?' do
@@ -99,7 +99,7 @@ end
 get '/?' do
   LOGGER.info "list all validations"
   content_type "text/uri-list"
-  Validation::Validation.all.collect{ |d| url_for("/", :full) + d.id.to_s }.join("\n")
+  Validation::Validation.all(params).collect{ |d| url_for("/", :full) + d.id.to_s }.join("\n")
 end
 
 get '/:id' do
